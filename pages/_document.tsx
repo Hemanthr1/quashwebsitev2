a3774404a82d3cadec4a6dfaa69126a4c51580b8
Hemanthr1 <hemanth@quashbugs.com>
2023-09-22 15:18:12 +0530

    head tag updated

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,11 @@
+import { Metadata } from "next";
 import { Html, Head, Main, NextScript } from "next/document";
+
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>🐞 Quash - Streamlining QA Processes 🚀</title>
         <link
           href="https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap"
           rel="stylesheet"
